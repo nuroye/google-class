5ce08565e7f6e6ad3388a7f86b49bdf3227075a7
nuroye <oyeleyenurudeen@yahoo.com>
2022-07-26 13:00:43 +0100

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import NavBar  from './components/NavBar';
+import NavBar from './components/NavBar';
 import Footer  from './components/Footer';
 import Routes  from './components/Routes';
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className={darkTheme ? 'dark': ''}>
        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-50 min-h-screen">
-        <NavBar/>
+        <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
         <Routes/>
         <Footer/>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  darkMode:'class',
   theme: {
     extend: {},
   },
